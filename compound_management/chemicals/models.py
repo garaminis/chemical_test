7f32b50678e1992069b697435a5725a6469ec6a7
hasn0737 <hasn0737@naver.com>
2024-07-25 17:56:45 +0900

    commit

--- compound_management/chemicals/models.py
+++ compound_management/chemicals/models.py
@@ -133,28 +133,28 @@ class CYPInhibition(models.Model):
         return f'{self.chemical.chem_id} - {self.date} CYP Inhibition'
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, name, password=None):
-        if not email:
-            raise ValueError('Must have user email')
+    def create_user(self, email, userID, password=None, **extra_fields):
 
-        user = self.model(
-            email=self.normalize_email(email),
-            name=name
-        )
+        if not email:
+            raise ValueError('The Email field must be set')
+        if not userID:
+            raise ValueError('The userID field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, userID=userID, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, name, password):
-        user = self.create_user(
-            email,
-            password=password,
-            name=name,
-        )
-        user.is_admin = True
-        user.is_staff = True
-        user.save(using=self._db)
-        return user
+    def create_superuser(self, email, userID, password=None, **extra_fields):
+
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(email, userID, password, **extra_fields)
 
 class User(AbstractBaseUser):
     id = models.AutoField(primary_key=True)
@@ -166,7 +166,7 @@ class User(AbstractBaseUser):
     date_joined = models.DateTimeField(default=timezone.now, blank=False)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     objects = UserManager()
     USERNAME_FIELD = 'userID'
